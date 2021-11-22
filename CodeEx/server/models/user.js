de549dff1ddead6f5f57f9e79466b15c8598ac6e
Hoàng Trung Nam <63184287+nampanama@users.noreply.github.com>
2021-11-22 17:05:31 +0700

    delete friend models

--- CodeEx/server/models/user.js
+++ CodeEx/server/models/user.js
@@ -5,9 +5,11 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
+    name: {type:String, required: true},
     avatar: {type: String, default: ''},
     email: {type: String, default: ''},
     country:{type: String, default: ''},
+    gender: {type: String, default: ''}
 },
 {
     timestamps: true
--- CodeEx/server/routes/index.js
+++ CodeEx/server/routes/index.js
@@ -1,13 +1,11 @@
 const siteController = require('../controllers/singleSites');
 const userRoute = require('./user');
 const postRoute = require('./post');
-const friendRoute = require('./friend');
 
 function routes(app) {
     app.get('/', siteController.index)
     app.use('/user', userRoute);
     app.use('/post', postRoute);
-    app.use('/friend', friendRoute);
 }
 
 module.exports = routes;
