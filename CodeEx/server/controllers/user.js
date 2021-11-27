a04d1117805c7092bc30ff735decf07629bc8059
Hoàng Trung Nam <63184287+nampanama@users.noreply.github.com>
2021-11-27 17:30:58 +0700

    update register and model user

--- CodeEx/server/controllers/user.js
+++ CodeEx/server/controllers/user.js
@@ -1,24 +1,34 @@
 const User = require('../models/user');
 const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const { ACCESS_TOKEN_SECRET } = process.env;
+
 
 class UserController {
     async register(req, res, next) {
-        const { username, password, name } = { ...req.body };
+        const { username, password, name, email } = { ...req.body };
         try {
-            if (!username || !password || !name) {
-                return res.status(401).json({ success: false, message: 'You have to enter username and password' });
+            if (!username || !password || !name || !email) {
+                return res.status(400).json({ success: false, message: 'You have to enter username and password' });
             }
             const user = await User.findOne({ username }).lean();
-
             if (user) {
-                return res.status(402).json({ success: false, message: 'This username already exists' });
+                return res.status(400).json({ success: false, message: 'This username already exists' });
             }
+            if(!validateEmail){
+                return res.status(400).json({ success: false, message: 'Invalid email'});
+            }
+
             const hashedPassword = await argon2.hash(password);
-            const newUser = new User({ username, password: hashedPassword, name });
+            const newUser = new User({ username, password: hashedPassword, name, email });
+            const accessToken = createAccessToken({newUser});
+            console.log(accessToken);
             await newUser.save();
-            return res.status(200).json({ success: true, message: 'Registration successful' });
+            return res.status(200).json({ success: true, message: 'Registration successful', accessToken });
         } catch (error) {
-            return res.status(500).json({ error: error });
+            return res.status(500).json({ error: error.message });
         }
     }
 
@@ -37,6 +47,17 @@ class UserController {
             return res.status(500).json({ error: error });
         }
     }
+}
+
+const validateEmail = (email) => {
+    return email.match(
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+
+const createAccessToken = (payload) => {
+    return jwt.sign(payload, ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
 }
 
 module.exports = new UserController;
--- CodeEx/server/models/user.js
+++ CodeEx/server/models/user.js
@@ -3,18 +3,19 @@ const mongoose_delete = require('mongoose-delete');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    username: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    name: {type:String, required: true},
-    avatar: {type: String, default: ''},
-    email: {type: String, default: ''},
-    country:{type: String, default: ''},
-    gender: {type: String, default: ''}
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    role: { type: Number, default: 0 },
+    avatar: { type: String, default: 'https://res.cloudinary.com/dxo2m1hlx/image/upload/v1637922613/icon/user-32_lg945e.png' },
+    email: { type: String, required: true},
+    country: { type: String, default: '' },
+    gender: { type: String, default: '' }
 },
-{
-    timestamps: true
-});
+    {
+        timestamps: true
+    });
 
-UserSchema.plugin(mongoose_delete, {overrideMethods: 'all'});
+UserSchema.plugin(mongoose_delete, { overrideMethods: 'all' });
 
 module.exports = mongoose.model('user', UserSchema);
