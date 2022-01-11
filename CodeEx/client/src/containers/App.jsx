371f49e4a5a425a045907d342e7e0401345a4de3
Phạm Kiên <trungkien2000.tx@gmail.com>
2022-01-11 13:53:01 +0700

    commit change

--- CodeEx/client/src/containers/App.jsx
+++ CodeEx/client/src/containers/App.jsx
@@ -2,12 +2,16 @@ import React from "react"
 import ChallengeDetail from "../components/challengeDetail/ChallengeDetail";
 import ChallengeJoined from "../components/challengeJoined/challengeJoined";
 import CreateChallenge from "../components/createChallenge/createChallenge";
-
+import { BrowserRouter as Router, Route, Switch, Link, NavLink } from 'react-router-dom'
 import Home from "./home/Home";
 import Homepage from "./homepage/Homepage";
 
 function App() {
     return (
+        // <Router>
+        //     <Route path="/" exact component={Homepage} />
+        //     <Route path="/home" component={Home} />
+        // </Router>
         <div>
             {/* <Homepage /> */}
             {/* <Home /> */}
@@ -15,6 +19,7 @@ function App() {
             {/* <CreateChallenge /> */}
             <ChallengeJoined />
         </div>
+
     )
 
 }
