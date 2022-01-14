35838d759c73c510cb889958f77c90975df09a81
Hoàng Trung Nam <63184287+nampanama@users.noreply.github.com>
2022-01-14 11:29:53 +0700

    update

--- CodeEx/client/src/containers/App.jsx
+++ CodeEx/client/src/containers/App.jsx
@@ -2,7 +2,6 @@ import React from "react"
 import ChallengeDetail from "../components/challengeDetail/ChallengeDetail";
 import ChallengeJoined from "../components/challengeJoined/challengeJoined";
 import CreateChallenge from "../components/createChallenge/createChallenge";
-// import { BrowserRouter as Router, Route, Switch, Link, NavLink } from 'react-router-dom'
 import Home from "./home/Home";
 import Homepage from "./homepage/Homepage";
 import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
